fa1f8e7ed36e368858ffe136f3f43739cd799e0c
Kilo <dev@kilocode.ai>
2026-05-07 20:13:43 +0700

    arrange table squence

--- drizzle/0000_boring_gargoyle.sql
+++ drizzle/0000_boring_gargoyle.sql
@@ -1,23 +1,21 @@
-CREATE TABLE `product_prices` (
-	`id` bigint AUTO_INCREMENT NOT NULL,
-	`variant_id` bigint NOT NULL,
-	`price_type` enum('retail','member','reseller') NOT NULL,
-	`price` decimal(12,2) NOT NULL,
-	`start_date` datetime,
-	`end_date` datetime,
-	CONSTRAINT `product_prices_id` PRIMARY KEY(`id`)
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` int AUTO_INCREMENT NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`password` varchar(255) NOT NULL,
+	`created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	CONSTRAINT `users_id` PRIMARY KEY(`id`),
+	CONSTRAINT `users_email_unique` UNIQUE(`email`)
 );
 --> statement-breakpoint
-CREATE TABLE `product_variants` (
-	`id` bigint AUTO_INCREMENT NOT NULL,
-	`product_id` bigint NOT NULL,
-	`sku` varchar(50) NOT NULL,
-	`variant_name` varchar(100),
-	`uom` varchar(10),
-	`is_active` boolean NOT NULL DEFAULT true,
-	`is_sellable` boolean NOT NULL DEFAULT true,
-	CONSTRAINT `product_variants_id` PRIMARY KEY(`id`),
-	CONSTRAINT `product_variants_sku_unique` UNIQUE(`sku`)
+CREATE TABLE `sessions` (
+	`id` int AUTO_INCREMENT NOT NULL,
+	`token` varchar(255) NOT NULL,
+	`user_id` int NOT NULL,
+	`created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT `sessions_id` PRIMARY KEY(`id`)
 );
 --> statement-breakpoint
 CREATE TABLE `products` (
@@ -32,23 +30,26 @@ CREATE TABLE `products` (
 	CONSTRAINT `products_product_id` PRIMARY KEY(`product_id`)
 );
 --> statement-breakpoint
-CREATE TABLE `sessions` (
-	`id` int AUTO_INCREMENT NOT NULL,
-	`token` varchar(255) NOT NULL,
-	`user_id` int NOT NULL,
-	`created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	CONSTRAINT `sessions_id` PRIMARY KEY(`id`)
+CREATE TABLE `product_variants` (
+	`id` bigint AUTO_INCREMENT NOT NULL,
+	`product_id` bigint NOT NULL,
+	`sku` varchar(50) NOT NULL,
+	`variant_name` varchar(100),
+	`uom` varchar(10),
+	`is_active` boolean NOT NULL DEFAULT true,
+	`is_sellable` boolean NOT NULL DEFAULT true,
+	CONSTRAINT `product_variants_id` PRIMARY KEY(`id`),
+	CONSTRAINT `product_variants_sku_unique` UNIQUE(`sku`)
 );
---> statement-breakpoint
-CREATE TABLE `users` (
-	`id` int AUTO_INCREMENT NOT NULL,
-	`name` varchar(255) NOT NULL,
-	`email` varchar(255) NOT NULL,
-	`password` varchar(255) NOT NULL,
-	`created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	CONSTRAINT `users_id` PRIMARY KEY(`id`),
-	CONSTRAINT `users_email_unique` UNIQUE(`email`)
+
+CREATE TABLE `product_prices` (
+	`id` bigint AUTO_INCREMENT NOT NULL,
+	`variant_id` bigint NOT NULL,
+	`price_type` enum('retail','member','reseller') NOT NULL,
+	`price` decimal(12,2) NOT NULL,
+	`start_date` datetime,
+	`end_date` datetime,
+	CONSTRAINT `product_prices_id` PRIMARY KEY(`id`)
 );
 --> statement-breakpoint
 CREATE TABLE `variant_attributes` (
